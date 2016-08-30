76db3e5a81806bdf069f1d4eac8456d6f1b1e620
Nikita Gusakov <gusnkt@gmail.com>
2016-08-30 13:01:32 +0300

    fix: export generator should add additional line at EOF

--- scripts/export.js
+++ scripts/export.js
@@ -4,8 +4,11 @@ const path = require('path');
 const src = path.resolve(__dirname, '../src/components');
 const exportFile = path.resolve(__dirname, '../src/index.js');
 
-const code = fs.readdirSync(src)
-  .map((name) => `export ${name} from './components/${name}/${name}.js';`)
-  .join('\n')
+
+const fileNames = fs.readdirSync(src);
+const codeLines = fileNames.map(
+  (name) => `export ${name} from './components/${name}/${name}.js';`
+);
+const code = codeLines.join('\n') + '\n';
 
 fs.writeFileSync(exportFile, code);
