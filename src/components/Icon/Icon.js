e62f153c78ea3324eadc0c610191d44d158d42a4
Oleg Shilov <oleg@dlg.im>
2016-09-23 18:00:00 +0300

    feat(Icon): add inverted and clickable icons with themes

--- src/components/Icon/Icon.js
+++ src/components/Icon/Icon.js
@@ -29,32 +29,39 @@ class Icon extends Component {
     /**
      * Click handler function, which will be executed when icon is clicked
      */
-    onClick: PropTypes.func
+    onClick: PropTypes.func,
+    theme: PropTypes.oneOf(['primary', 'success', 'danger', 'info', 'warning']),
+    inverted: PropTypes.bool
+  };
+
+  static defaultProps = {
+    inverted: false
   };
 
   shouldComponentUpdate(nextProps) {
     return nextProps.glyph !== this.props.glyph ||
            nextProps.onClick !== this.props.onClick ||
+           nextProps.theme !== this.props.theme ||
+           nextProps.inverted !== this.props.inverted ||
            nextProps.className !== this.props.className;
   }
 
   renderMaterialIcon() {
-    const { glyph, className, onClick } = this.props;
-    const iconClassName = classNames('material-icons', styles.material, className);
+    const { glyph } = this.props;
+    const className = classNames('material-icons', styles.material);
 
     return (
-      <i className={iconClassName} onClick={onClick}>{glyph}</i>
+      <i className={className}>{glyph}</i>
     );
   }
 
   renderSvgIcon() {
-    const { glyph, className, onClick } = this.props;
-    const svgClassName = classNames(styles.svg, className);
+    const { glyph } = this.props;
 
     switch (glyph) {
       case 'apple':
         return (
-          <svg viewBox="0 0 32 32" className={svgClassName} onClick={onClick}>
+          <svg viewBox="0 0 32 32" className={styles.svg}>
             <g transform="translate(2.000000, 1.000000)">
               <path d="M24.3496597,9.88322937 C22.7905668,7.61011118 20.3740244,7.29953079 19.5248295,7.27476691 C17.4952228,7.0611784 15.526494,8.48922907 14.4926018,8.48922907 C13.4380731,8.48922907 11.8459616,7.29540348 10.1300309,7.33048564 C7.92191781,7.36453599 5.85619712,8.64297153 4.7232494,10.6282096 C2.38512598,14.6760728 4.12891617,20.6245643 6.36901589,23.8964925 C7.48958166,25.4989222 8.79897205,27.2881128 10.5128392,27.2241395 C12.1895606,27.1560388 12.8158805,26.1561969 14.8392962,26.1561969 C16.844139,26.1561969 17.4322813,27.2241395 19.1801988,27.1838982 C20.9797077,27.1560388 22.1126554,25.5752775 23.1940117,23.9573704 C24.4889565,22.1217475 25.0089981,20.3119202 25.0296346,20.2190557 C24.9883615,20.2056419 21.5358633,18.8869651 21.5007811,14.9020433 C21.4708581,11.57646 24.2237766,9.95855289 24.3496597,9.88322937 L24.3496597,9.88322937 Z M17.0495121,5.12237262 C17.9513301,3.99458405 18.5683636,2.46025505 18.3970801,0.903225806 C17.091817,0.961008205 15.4594642,1.80607576 14.5194685,2.90910045 C13.6878147,3.88108291 12.9448982,5.47422614 13.1368183,6.97244115 C14.6030466,7.08181497 16.1084845,6.2326201 17.0495121,5.12237262 L17.0495121,5.12237262 Z" />
             </g>
@@ -62,13 +69,13 @@ class Icon extends Component {
         );
       case 'logo':
         return (
-          <svg viewBox="0 0 32 32" className={svgClassName} onClick={onClick}>
+          <svg viewBox="0 0 32 32" className={styles.svg}>
             <path d="M7.39664743,5.39698047 C11.9259548,0.867673177 19.2692052,0.867673177 23.7985125,5.39698047 C28.3278197,9.92673181 28.3278197,17.2690942 23.7985125,21.7984014 L15.597358,30 L15.597358,25.195382 C12.6293295,25.195382 9.66130108,24.0630551 7.39664743,21.7984014 C2.86778419,17.2699822 2.86778419,9.92673181 7.39664743,5.39698047" transform="translate(0, 2)" />
           </svg>
         );
       case 'phone':
         return (
-          <svg viewBox="0 0 32 32" className={svgClassName} onClick={onClick}>
+          <svg viewBox="0 0 32 32" className={styles.svg}>
             <g transform="translate(3.000000, 2.000000)">
               <path d="M14.5022222,22.4325926 L18.0812721,24.1988748 C18.4412722,23.8388749 18.3288889,19.0192593 18.7955555,19.1792592 C20.2888889,19.6725926 21.9022222,19.9392593 23.5555555,19.9392593 C24.2888889,19.9392593 24.8888889,20.5392593 24.8888889,21.2725925 L23.7336158,25.1560689 C23.7336158,25.8894022 23.7153216,25.925927 22.9819883,25.925927 C22.1906867,25.925927 21.9824331,27.2187272 21.2120552,27.1396256 C10.8614636,26.0768378 2.57657017,18.0515217 1.11734745,7.82364991 C0.966784643,6.76833663 1.29709201,5.68111108 1.29709201,4.58412904 C1.29709201,4.31698114 1.21719272,4.95890428 1.67005603,5.87225504 C1.75273533,6.03900559 1.83956642,6.21508542 1.92930945,6.39671722 C2.69735047,7.95116312 3.6786707,9.91225407 4.09608289,9.91225407 L5.71555555,13.6459259 C7.63555551,17.4192593 10.7288889,20.4992593 14.5022222,22.4325926 L14.5022222,22.4325926 Z" />
               <path d="M6.36724192,11.61131 C9.68095043,11.61131 11.5578345,7.77602359 11.0147028,4.63588687 C10.4715713,1.49575014 8.49777569,0.604962379 5.1886936,0.604962379 C1.87961152,0.604962379 0.594292533,4.28058088 1.0985243,7.45310692 C1.60275608,10.625633 3.05353341,11.61131 6.36724192,11.61131 L6.36724192,11.61131 Z" />
@@ -78,7 +85,7 @@ class Icon extends Component {
         );
       case 'more':
         return (
-          <svg viewBox="0 0 32 32" className={svgClassName} onClick={onClick}>
+          <svg viewBox="0 0 32 32" className={styles.svg}>
             <g transform="translate(7.000000, 14.000000)">
               <circle cx="2.5" cy="2.5" r="2.5" />
               <circle cx="9.5" cy="2.5" r="2.5" />
@@ -91,7 +98,7 @@ class Icon extends Component {
     }
   }
 
-  render() {
+  renderGlyph() {
     const { glyph } = this.props;
 
     switch (glyph) {
@@ -102,8 +109,23 @@ class Icon extends Component {
       case 'more':
         return this.renderSvgIcon();
       default:
-        return this.renderMaterialIcon();
+        return this.renderMaterialIcon()
     }
+  }
+
+  render() {
+    const { onClick, theme, inverted } = this.props;
+    const className = classNames(styles.container, {
+      [styles[theme]]: theme,
+      [styles.inverted]: inverted,
+      [styles.clickable]: onClick
+    }, this.props.className);
+
+    return (
+      <div className={className} onClick={onClick}>
+        {this.renderGlyph()}
+      </div>
+    );
   }
 }
 
--- src/components/Icon/README.md
+++ src/components/Icon/README.md
@@ -2,21 +2,50 @@ Basic Icon:
 
 ```
 <div>
+  <Icon glyph="logo" />
   <Icon glyph="done" />
   <Icon glyph="settings" />
   <Icon glyph="call" />
   <Icon glyph="clear" />
   <Icon glyph="place" />
-</div>
-```
-
-Our SVG Icons:
-
-```
-<div>
-  <Icon glyph="logo" />
   <Icon glyph="phone" />
   <Icon glyph="apple" />
   <Icon glyph="more" />
+</div>
+```
+
+Themed Icons:
+
+```
+<div>
+  <Icon glyph="logo" theme="primary" />
+  <Icon glyph="done" theme="success" />
+  <Icon glyph="settings" theme="danger" />
+  <Icon glyph="call" theme="info" />
+  <Icon glyph="clear" theme="warning" />
+</div>
+```
+
+Inverted Icon:
+
+```
+<div>
+  <Icon glyph="logo" theme="primary" inverted />
+  <Icon glyph="done" theme="success" inverted />
+  <Icon glyph="settings" theme="danger" inverted />
+  <Icon glyph="call" theme="info" inverted />
+  <Icon glyph="clear" theme="warning" inverted />
+</div>
+```
+
+Clickable Icon:
+
+```
+<div>
+  <Icon glyph="logo" theme="primary" inverted onClick={() => {}} />
+  <Icon glyph="done" theme="success" onClick={() => {}} />
+  <Icon glyph="settings" theme="danger" inverted onClick={() => {}} />
+  <Icon glyph="call" theme="info" onClick={() => {}} />
+  <Icon glyph="clear" theme="warning" inverted onClick={() => {}} />
 </div>
 ```
