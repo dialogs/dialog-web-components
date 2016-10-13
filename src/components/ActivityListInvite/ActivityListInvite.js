7b0af3b4f4e0320ddddb7cc813f6489e1056f635
Oleg Shilov <oleg@dlg.im>
2016-10-13 16:50:09 +0300

    feat(ActivityListInvite): add stateless component

---
+++ src/components/ActivityListInvite/ActivityListInvite.js
@@ -0,0 +1,39 @@
+/**
+ * Copyright 2016 Dialog LLC <info@dlg.im>
+ * @flow
+ */
+
+import React from 'react';
+import { Text } from '@dlghq/react-l10n';
+import classNames from 'classnames';
+import ActivityListItem from '../ActivityListItem/ActivityListItem';
+import Icon from '../Icon/Icon';
+import styles from './ActivityListInvite.css';
+
+export type Props = {
+  className?: string,
+  onClick: () => any
+};
+
+function ActivityListInvite(props: Props): React.Element<any> {
+  const className = classNames(styles.container, props.className);
+
+  return (
+    <ActivityListItem className={className} onClick={props.onClick}>
+      <Icon
+        glyph="link"
+        inverted
+        theme="success"
+        className={styles.icon}
+      />
+      <Text
+        tagName="div"
+        id={'ActivityListInvite.invite'}
+        className={styles.text}
+      />
+      <Icon glyph="keyboard_arrow_right" className={styles.arrow} />
+    </ActivityListItem>
+  );
+}
+
+export default ActivityListInvite;
