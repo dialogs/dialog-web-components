736e4f0daaaede2ab0ce46c17a73bb9c285bc777
Oleg Shilov <oleg@dlg.im>
2016-10-19 20:24:25 +0300

    fix(Utils): add multiple flag

--- src/utils/selectFiles.js
+++ src/utils/selectFiles.js
@@ -3,10 +3,10 @@
  * @flow
  */
 
-function selectFiles(callback: (files: File[]) => void) {
+function selectFiles(callback: (files: File[]) => void, multiple?: boolean) {
   const input = document.createElement('input');
   input.type = 'file';
-  input.multiple = true;
+  input.multiple = multiple || true;
   input.onchange = () => {
     input.onchange = null;
     callback(Array.from(input.files));
