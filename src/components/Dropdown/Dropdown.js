600e79a502813cd20c02b58fe8abce6dd8b6b84b
Oleg Shilov <oleg@dlg.im>
2016-12-09 20:25:56 +0300

    feat(Dropdown): add mounting animation

--- src/components/Dropdown/Dropdown.js
+++ src/components/Dropdown/Dropdown.js
@@ -5,6 +5,7 @@
 
 import React, { PureComponent } from 'react';
 import classNames from 'classnames';
+import CSSTransitionGroup from 'react-addons-css-transition-group';
 import styles from './Dropdown.css';
 
 export type Props = {
@@ -26,9 +27,20 @@ class Dropdown extends PureComponent {
     const className = classNames(styles.container, styles[theme], this.props.className);
 
     return (
-      <div className={className} style={style}>
-        {this.props.children}
-      </div>
+      <CSSTransitionGroup
+        transitionAppear={true}
+        transitionAppearTimeout={150}
+        transitionEnter={false}
+        transitionLeave={false}
+        transitionName={{
+          appear: styles.appear,
+          appearActive: styles.appearActive,
+        }}
+      >
+        <div className={className} style={style}>
+          {this.props.children}
+        </div>
+      </CSSTransitionGroup>
     );
   }
 }
