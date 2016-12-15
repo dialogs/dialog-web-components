4cda85af3644310e842808ec09d7274e848e1853
Oleg Shilov <oleg@dlg.im>
2016-12-15 05:01:23 +0300

    fix(EmojiList): add transport category to emoji list

--- src/components/EmojiList/categories.js
+++ src/components/EmojiList/categories.js
@@ -9,6 +9,7 @@ export default [
   { name: 'nature', glyph: 'emoji_nature', chars: categories.nature },
   { name: 'foods', glyph: 'emoji_food', chars: categories.foods },
   { name: 'objects', glyph: 'emoji_party', chars: categories.objects },
+  { name: 'travel', glyph: 'emoji_travel', chars: categories.places },
   { name: 'activity', glyph: 'emoji_activity', chars: categories.activity },
   { name: 'symbols', glyph: 'emoji_objects', chars: [...categories.symbols, ...categories.flags] }
 ];
