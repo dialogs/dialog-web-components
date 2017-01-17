44382d72b85828e08081059e1679e1710828e6ac
Oleg Shilov <oleg@dlg.im>
2017-01-17 23:34:38 +0300

    fix(EmojiButton): extends from PureComponent, add click handler which send char to onClick prop

--- src/components/EmojiButton/EmojiButton.js
+++ src/components/EmojiButton/EmojiButton.js
@@ -3,7 +3,7 @@
  * @flow
  */
 
-import React from 'react';
+import React, { PureComponent } from 'react';
 import classNames from 'classnames';
 import Emoji from '../Emoji/Emoji';
 import styles from './EmojiButton.css';
@@ -12,21 +12,29 @@ export type Props = {
   className?: string,
   count: number,
   char: string,
-  onClick: () => void,
+  onClick: (char: string) => void,
   active: boolean
 };
 
-function EmojiButton(props: Props): React.Element<any> {
-  const className = classNames(styles.container, {
-    [styles.active]: props.active
-  }, props.className);
+class EmojiButton extends PureComponent {
+  props: Props;
 
-  return (
-    <button className={className} onClick={props.onClick}>
-      <Emoji className={styles.emoji} char={props.char} size="18" />
-      <span className={styles.counter}>{props.count}</span>
-    </button>
-  );
+  handleClick = (): void => {
+    this.props.onClick(this.props.char);
+  };
+
+  render(): React.Element<any> {
+    const className = classNames(styles.container, {
+      [styles.active]: this.props.active
+    }, this.props.className);
+
+    return (
+      <button className={className} onClick={this.handleClick}>
+        <Emoji className={styles.emoji} char={this.props.char} size="16" />
+        <span className={styles.counter}>{this.props.count}</span>
+      </button>
+    );
+  }
 }
 
 export default EmojiButton;
