1f200114932d883e046625552e5390e9f8cd5865
Nikita Gusakov <gusnkt@gmail.com>
2017-01-24 14:34:53 +0300

    feat(Header): disable hover when user selecting something

--- src/components/Hover/Hover.js
+++ src/components/Hover/Hover.js
@@ -4,6 +4,7 @@
  */
 
 import React, { Component } from 'react';
+import { listen } from '@dlghq/dialog-utils';
 
 export type Props = {
   className?: string,
@@ -13,13 +14,53 @@ export type Props = {
 
 class Hover extends Component {
   props: Props;
+  hover: boolean;
+  selecting: boolean;
+  listeners: ?Array<{ remove(): void }>;
+
+  constructor(props: Props) {
+    super(props);
+
+    this.hover = false;
+    this.selecting = false;
+  }
+
+  componentDidMount() {
+    this.listeners = [
+      listen(document, 'selectionchange', this.handleSelectionChange, { passive: true })
+    ];
+  }
+
+  componentWillUnmount() {
+    if (this.listeners) {
+      this.listeners.forEach((listener) => listener.remove());
+      this.listeners = null;
+    }
+  }
+
+  handleSelectionChange = (event) => {
+    const selection = document.getSelection();
+    if (selection && selection.toString()) {
+      this.selecting = true;
+      this.props.onHover(false);
+    } else {
+      this.selecting = false;
+      this.props.onHover(this.hover);
+    }
+  };
 
   handleMouseEnter = (): void => {
-    this.props.onHover(true);
+    this.hover = true;
+    if (!this.selecting) {
+      this.props.onHover(true);
+    }
   };
 
   handleMouseLeave = (): void => {
-    this.props.onHover(false);
+    this.hover = false;
+    if (!this.selecting) {
+      this.props.onHover(false);
+    }
   };
 
   render() {
