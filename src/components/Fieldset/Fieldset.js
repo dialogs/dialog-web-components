6ac11e635b24b92a844ae47ecf449c37b3d9f429
Nikita Gusakov <gusnkt@gmail.com>
2017-02-13 16:31:52 +0300

    fix(Fieldset): add flow support

--- src/components/Fieldset/Fieldset.js
+++ src/components/Fieldset/Fieldset.js
@@ -1,31 +1,28 @@
-import React, { Component, PropTypes } from 'react';
+/**
+ * Copyright 2017 dialog LLC <info@dlg.im>
+ * @flow
+ */
+
+import React from 'react';
 import { Text } from '@dlghq/react-l10n';
 import classNames from 'classnames';
 import styles from './Fieldset.css';
 
-class Fieldset extends Component {
-  static propTypes = {
-    className: PropTypes.string,
-    legend: PropTypes.string.isRequired,
-    children: PropTypes.node.isRequired
-  };
+type Props = {
+  className?: string,
+  legend: string,
+  children?: mixed
+};
 
-  shouldComponentUpdate(nextProps) {
-    return nextProps.children !== this.props.children ||
-           nextProps.legend !== this.props.legend ||
-           nextProps.className !== this.props.className;
-  }
+function Fieldset(props: Props) {
+  const className = classNames(styles.container, props.className);
 
-  render() {
-    const className = classNames(styles.container, this.props.className);
-
-    return (
-      <fieldset className={className}>
-        <Text id={this.props.legend} className={styles.legend} tagName="legend" />
-        {this.props.children}
-      </fieldset>
-    );
-  }
+  return (
+    <fieldset className={className}>
+      <Text id={props.legend} className={styles.legend} tagName="legend" />
+      {props.children}
+    </fieldset>
+  );
 }
 
 export default Fieldset;
