e0511ea6022f6963e7a91ee9420055a662e1f8d5
Oleg Shilov <oleg@dlg.im>
2017-03-21 21:04:53 +0300

    feat(AuthorizationForm): add component for select login type, move types to separate file

---
+++ src/components/AuthorizationForm/LoginTypeSelector.js
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2017 dialog LLC <info@dlg.im>
+ * @flow
+ */
+
+import type { AuthType } from './types';
+import React, { PureComponent } from 'react';
+import { Text } from '@dlghq/react-l10n';
+import Radio from '../Radio/Radio';
+import RadioGroup from '../Radio/RadioGroup';
+import styles from './AuthorizationForm.css';
+
+export type Props = {
+  type: AuthType,
+  allowed: AuthType[],
+  onTypeChange: (type: AuthType) => any
+};
+
+class LoginTypeSelector extends PureComponent {
+  props: Props;
+
+  renderOptions() {
+    return this.props.allowed.map((type) => {
+      return (
+        <Radio value={type} key={type} className={styles.type}>
+          <Text id={`AuthorizationForm.type.${type}`} />
+        </Radio>
+      );
+    });
+  }
+
+  render() {
+    return (
+      <RadioGroup name="login_type" value={this.props.type} onChange={this.props.onTypeChange}>
+        {this.renderOptions()}
+      </RadioGroup>
+    );
+  }
+}
+
+export default LoginTypeSelector;
