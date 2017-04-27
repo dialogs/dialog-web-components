3201561b1f4e512399c73494105b83db26da73cc
Oleg Shilov <oleg@dlg.im>
2017-04-27 21:10:21 +0300

    fix(getCountries): return value instead method

--- src/utils/getCountries.js
+++ src/utils/getCountries.js
@@ -3,24 +3,18 @@
  * @flow
  */
 
-import type { Country } from '../components/CountryCodeSelector/types';
-import { getCountries as getCountriesFromLib } from '@dlghq/country-codes';
+import { getCountries } from '@dlghq/country-codes';
 
-function getCountries(): Country[] {
-  const countries = [];
+const countries = [];
 
-  getCountriesFromLib().forEach((country) => {
-    country.codes.forEach((code) => {
-      countries.push({
-        alpha: country.alpha,
-        code,
-        flag: country.emoji
-      });
+getCountries().forEach((country) => {
+  country.codes.forEach((code) => {
+    countries.push({
+      alpha: country.alpha,
+      code,
+      flag: country.emoji
     });
   });
+});
 
-  return countries;
-}
-
-
-export default getCountries;
+export default countries;
