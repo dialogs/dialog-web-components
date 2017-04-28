0c9ff8d9becde064ff86009c829040288acbc8c3
Oleg Shilov <oleg@dlg.im>
2017-04-28 21:43:54 +0300

    fix(MessageContent): fixed eslint and flow errors

--- src/components/MessageContent/Document/Document.js
+++ src/components/MessageContent/Document/Document.js
@@ -13,19 +13,15 @@ type Props = {
   fileUrl: ?string,
   fileName: ?string,
   fileSize: ?string,
-  fileExtension: ?string,
+  // fileExtension: ?string,
   isUploading: boolean
 };
 
 class Document extends PureComponent {
   props: Props;
 
-  handleDownload = (): void => {
-    console.debug('handleDownload');
-  };
-
   render() {
-    const { fileUrl, fileName, fileSize, fileExtension, isUploading } = this.props;
+    const { fileUrl, fileName, fileSize, isUploading } = this.props;
     const className = classNames(styles.container, this.props.className, {
       [styles.uploading]: isUploading
     });
@@ -35,7 +31,7 @@ class Document extends PureComponent {
 
     return (
       <tagName className={className} {...tagProps}>
-        <DownloadButton isUploading={isUploading} onClick={this.handleDownload} />
+        <DownloadButton isUploading={isUploading} />
         <div className={styles.info}>
           <div className={styles.filename}>
             <div className={styles.text} title={fileName}>{fileName}</div>
