79e9ab7467c6cb2bb7b71fd0ac121773c6132239
Nikita Gusakov <gusnkt@gmail.com>
2017-11-10 14:06:00 +0300

    fix(MediaErrorMessage): support non-browser environment

--- src/components/MediaErrorMessage/MediaErrorMessage.js
+++ src/components/MediaErrorMessage/MediaErrorMessage.js
@@ -10,13 +10,17 @@ type Props = {
   error: MediaError
 };
 
-const MEDIA_ERR_ABORTED = typeof MediaError.MEDIA_ERR_ABORTED === 'number' ? MediaError.MEDIA_ERR_ABORTED : 1;
-const MEDIA_ERR_NETWORK = typeof MediaError.MEDIA_ERR_NETWORK === 'number' ? MediaError.MEDIA_ERR_NETWORK : 2;
-const MEDIA_ERR_DECODE = typeof MediaError.MEDIA_ERR_DECODE === 'number' ? MediaError.MEDIA_ERR_DECODE : 3;
-const MEDIA_ERR_SRC_NOT_SUPPORTED =
-  typeof MediaError.MEDIA_ERR_SRC_NOT_SUPPORTED === 'number' ? MediaError.MEDIA_ERR_SRC_NOT_SUPPORTED : 4;
-
 function getCode(error: MediaError): string {
+  if (typeof MediaError === 'undefined') {
+    return 'MEDIA_ERR_UNKNOWN';
+  }
+
+  const MEDIA_ERR_ABORTED = typeof MediaError.MEDIA_ERR_ABORTED === 'number' ? MediaError.MEDIA_ERR_ABORTED : 1;
+  const MEDIA_ERR_NETWORK = typeof MediaError.MEDIA_ERR_NETWORK === 'number' ? MediaError.MEDIA_ERR_NETWORK : 2;
+  const MEDIA_ERR_DECODE = typeof MediaError.MEDIA_ERR_DECODE === 'number' ? MediaError.MEDIA_ERR_DECODE : 3;
+  const MEDIA_ERR_SRC_NOT_SUPPORTED =
+    typeof MediaError.MEDIA_ERR_SRC_NOT_SUPPORTED === 'number' ? MediaError.MEDIA_ERR_SRC_NOT_SUPPORTED : 4;
+
   switch (error.code) {
     case MEDIA_ERR_ABORTED:
       return 'MEDIA_ERR_ABORTED';
