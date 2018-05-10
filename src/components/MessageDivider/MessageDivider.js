858054fd13ef166fcc142a3886029d4150f83a3e
Oleg Shilov <oleg@dlg.im>
2018-05-10 19:17:51 +0300

    feat(MessageDivider): add visible prop and add show/hide animations

--- src/components/MessageDivider/MessageDivider.js
+++ src/components/MessageDivider/MessageDivider.js
@@ -6,28 +6,45 @@
 import type { ColorTheme } from '@dlghq/dialog-types';
 import React, { PureComponent, type Node } from 'react';
 import classNames from 'classnames';
+import { CSSTransition } from 'react-transition-group';
 import styles from './MessageDivider.css';
 
 export type Props = {
   className?: string,
   theme: ColorTheme,
-  children: Node
+  children: Node,
+  visible: boolean
 };
 
 class MessageDivider extends PureComponent<Props> {
   static defaultProps = {
-    theme: 'default'
+    theme: 'default',
+    visible: true
   };
 
   render() {
     const className = classNames(styles.container, styles[this.props.theme], this.props.className);
 
     return (
-      <div className={className}>
-        <div className={styles.text}>
-          {this.props.children}
+      <CSSTransition
+        classNames={{
+          enter: styles.enter,
+          enterActive: styles.enterActive,
+          leave: styles.leave,
+          exitActive: styles.leaveActive
+        }}
+        timeout={{
+          enter: 300, exit: 300
+        }}
+        unmountOnExit
+        in={this.props.visible}
+      >
+        <div className={className}>
+          <div className={styles.text}>
+            {this.props.children}
+          </div>
         </div>
-      </div>
+      </CSSTransition>
     );
   }
 }
--- src/components/MessageDivider/README.md
+++ src/components/MessageDivider/README.md
@@ -1,8 +1,17 @@
 Basic MessageDivider:
 
 ```jsx
+initialState = {
+  isVisible: true
+};
+
 <div>
-  <MessageDivider>Today</MessageDivider>
+  <Button theme="primary" onClick={() => setState({ isVisible: !state.isVisible })} size="small">
+    Toggle
+  </Button>
+  <br />
+  <br />
+  <MessageDivider visible={state.isVisible} theme="primary">Today</MessageDivider>
 </div>
 ```
 
@@ -10,6 +19,7 @@ Themable MessageDivider:
 
 ```jsx
 <div>
+  <MessageDivider>Default</MessageDivider>
   <MessageDivider theme="primary">Primary</MessageDivider>
   <MessageDivider theme="success">Success</MessageDivider>
   <MessageDivider theme="danger">Danger</MessageDivider>
