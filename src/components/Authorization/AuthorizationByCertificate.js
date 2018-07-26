a61e0d55b47896861ed7227d238ab5c33a3f18a8
Oleg Shilov <oleg@dlg.im>
2018-07-26 03:46:57 +0300

    fix(Authorization): render error message for cert auth

--- src/components/Authorization/AuthorizationByCertificate.js
+++ src/components/Authorization/AuthorizationByCertificate.js
@@ -3,17 +3,44 @@
  * @flow
  */
 
-// import type { AuthError } from '@dlghq/dialog-types';
-import React from 'react';
+import type { AuthError } from '@dlghq/dialog-types';
+import React, { PureComponent } from 'react';
+import { Text } from '@dlghq/react-l10n';
+import Error from '../Error/Error';
 import styles from './Authorization.css';
 
 export type Props = {
   id: string,
-  // errors: ?{ [field: string]: AuthError }
+  errors: ?{ [field: string]: AuthError }
 };
 
-function AuthorizationByCertificate(props: Props) {
-  return <div className={styles.formWrapper} id={props.id} />;
+class AuthorizationByCertificate extends PureComponent<Props> {
+  renderErrors() {
+    const { errors } = this.props;
+
+    if (!(errors && errors.cert)) {
+      return null;
+    }
+
+    return (
+      <div className={styles.errorWrapper}>
+        <Error>
+          <Text id="Authorization.cert_error" html />
+        </Error>
+      </div>
+    );
+  }
+
+  render() {
+    const { id } = this.props;
+    console.log(this.props);
+
+    return (
+      <div className={styles.formWrapper} id={id}>
+        {this.renderErrors()}
+      </div>
+    );
+  }
 }
 
 export default AuthorizationByCertificate;
