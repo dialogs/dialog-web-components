c64235c3ee879558b60f47620b1b88e6fa023aa4
Max Stepanov <epolezhaev@icerockdev.com>
2018-11-02 12:42:50 +0700

    refactor(AudioPlayer): change decode lib from ogv.js to opus-recorder

--- src/components/AudioPlayer/AudioPlayer.js
+++ src/components/AudioPlayer/AudioPlayer.js
@@ -10,19 +10,13 @@ import MediaErrorMessage from '../MediaErrorMessage/MediaErrorMessage';
 import AudioPlayerButton from './AudioPlayerButton/AudioPlayerButton';
 import PeerInfoTitle from '../PeerInfoTitle/PeerInfoTitle';
 import styles from './AudioPlayer.css';
-import ogv from 'ogv';
-
-if (navigator.userAgent.toLowerCase().indexOf(' electron/') > -1) {
-  ogv.OGVLoader.base = process.resourcesPath + '/ogv';
-} else {
-  ogv.OGVLoader.base = 'ogv';
-}
+import Recorder from 'opus-recorder';
 
 type Props = {
   src: ?string,
   duration?: ?number,
   pending?: ?boolean,
-  sender?: ?string
+  sender?: ?string,
 };
 
 type State = {
@@ -30,15 +24,23 @@ type State = {
   error: ?MediaError,
   duration: number,
   isPlaying: boolean,
-  isEnded: boolean,
   currentTime: number,
-  isOgg: boolean
+  decodedSrc: string,
+  isDecoded: boolean,
+};
+
+const options = {
+  sampleRate: 48000,
+  wavBitDepth: 32,
+  decoderWorkerPath: '/devapp/decoderWorker.min.js',
+  wavWorkerPath: '/devapp/waveWorker.min.js'
 };
 
 class AudioPlayer extends PureComponent<Props, State> {
   audio: ?HTMLMediaElement;
   rewind: ?HTMLElement;
-  playerOGV: ?HTMLElement;
+  decodeWorker: Worker;
+  wavWorker: Worker;
 
   constructor(props: Props) {
     super(props);
@@ -48,32 +50,41 @@ class AudioPlayer extends PureComponent<Props, State> {
       error: null,
       duration: props.duration || 0,
       isPlaying: false,
-      isEnded: false,
       currentTime: 0,
-      isOgg: true
+      decodedSrc: '',
+      isDecoded: false
     };
   }
 
   handleError = () => {
-    const { isOgg } = this.state;
-    if (isOgg && this.playerOGV.error) {
-      this.setState({
-        error: this.playerOGV.error.message,
-        isPlaying: false,
-        currentTime: 0
-      });
-    } else {
+    if (this.audio) {
       const { error } = this.audio;
 
       this.setState({
         error,
         isPlaying: false,
-        currentTime: 0
+        currentTime: 0,
       });
 
       console.error(error); // eslint-disable-line
     }
   };
+
+  componentWillMount() {
+    const { src } = this.props;
+
+    if(!src){
+      return;
+    }
+
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", src, true);
+    xhr.responseType = "arraybuffer";
+    xhr.onload = (e) => {
+      this.decodeAudio(xhr.response);
+    };
+    xhr.send();
+  }
 
   handleLoadedMetadata = () => {
     const duration = this.getDuration();
@@ -81,92 +92,51 @@ class AudioPlayer extends PureComponent<Props, State> {
   };
 
   handlePlayClick = () => {
-    const { isEnded, isOgg } = this.state;
-    if (isEnded && isOgg) {
-      this.playerOGV.stop();
-    }
-
     this.setState(() => {
-      if (this.playerOGV.error && isOgg) {
-        return {
-          key: String(Math.random()),
-          error: this.playerOGV.error.message,
-          isPlaying: false,
-          currentTime: 0
-        };
+      if (this.audio) {
+        if (this.audio.error) {
+          return {
+            key: String(Math.random()),
+            error: null,
+            isPlaying: true,
+            currentTime: 0,
+          };
+        }
+
+        this.audio.play();
       }
 
-      return {
-        isPlaying: true,
-        isEnded: false
-      };
+      return { isPlaying: true };
     });
-
-    if (!isOgg && this.audio) {
-      this.audio.play();
-    }
-
-    if (this.playerOGV) {
-      this.playerOGV.play();
-    }
   };
 
   handlePauseClick = () => {
-    const { isOgg } = this.state;
-
-    this.setState({
-      isPlaying: false
-    });
-    if (this.playerOGV) {
-      this.playerOGV.pause();
-    }
-    if (isOgg) {
-      this.playerOGV.pause();
-    } else {
+    if (this.audio) {
       this.audio.pause();
     }
   };
 
   handleEnded = () => {
-    const { isOgg } = this.state;
-    const { duration } = this.props;
+    const currentTime = this.getCurrentTime();
+    this.setState({ currentTime, isPlaying: false });
+  };
 
-    if (isOgg) {
-      this.playerOGV.currentTime = duration;
-    }
-
-    this.setState({
-      isPlaying: false,
-      currentTime: duration,
-      isEnded: true
-    });
+  handlePause = () => {
+    const currentTime = this.getCurrentTime();
+    this.setState({ currentTime, isPlaying: false });
   };
 
   handleRewind = (event: SyntheticMouseEvent<>) => {
-    const { isEnded, isOgg } = this.state;
-    const { duration } = this.props;
-
     event.stopPropagation();
-    if (!this.rewind && this.state.error) {
-      return;
-    }
 
-    const rewindRect = this.rewind.getBoundingClientRect();
-    const rewindPosition = (event.clientX - rewindRect.left) / rewindRect.width;
-    if (isOgg) {
-      if (isEnded) {
-        this.setState({
-          isEnded: false
-        });
+    if (this.rewind && !this.state.error) {
+      const rewindRect = this.rewind.getBoundingClientRect();
+      const rewindPosition =
+        (event.clientX - rewindRect.left) / rewindRect.width;
+
+      if (this.audio) {
+        this.audio.currentTime = this.audio.duration * rewindPosition;
       }
-      if (this.playerOGV) {
-        this.playerOGV.currentTime = (duration * rewindPosition) / 1000;
-        this.setState({
-          currentTime: this.playerOGV.currentTime.toFixed(5)
-        });
-      }
-    } else {
-      this.audio.currentTime = this.audio.duration * rewindPosition;
     }
   };
 
@@ -184,52 +154,22 @@ class AudioPlayer extends PureComponent<Props, State> {
   }
 
   getCurrentTime(): number {
-    if (this.audio && !this.state.isOgg) {
-      return parseInt(this.audio.currentTime, 10);
+    if (this.audio) {
+      return parseInt(this.audio.currentTime * 1000, 10);
     }
 
     return 0;
   }
 
   setAudio = (audio: ?HTMLMediaElement) => {
-    const { src } = this.props;
-    if (!src) {
-      return;
-    }
-
-    if (!this.playerOGV) {
-      this.playerOGV = new ogv.OGVPlayer();
-      this.playerOGV.src = this.props.src;
-
-      this.playerOGV.addEventListener('ended', this.handleEnded);
-      this.playerOGV.addEventListener('playing', this.updateCurrentTime);
-      this.playerOGV.addEventListener('error', this.handleError);
-    }
-
-    let format = src.split('?')[0].split('.');
-    format = format[format.length - 1];
-
-    this.setState(() => {
-      if (format === 'ogg' || format === 'opus') {
-        return {
-          isOgg: true
-        };
-      }
-
-      return {
-        isOgg: false
-      };
-    });
-
-    if ((format !== 'ogg' || format !== 'opus') && audio) {
-      this.audio = audio;
-    }
-
     if (audio) {
       audio.volume = 1;
-    } else {
-      this.playerOGV.volume = 1;
+      if (this.state.isPlaying) {
+        audio.play();
+      }
     }
+
+    this.audio = audio;
   };
 
   setRewind = (rewind: ?HTMLElement) => {
@@ -238,66 +178,98 @@ class AudioPlayer extends PureComponent<Props, State> {
     }
   };
 
-  updateCurrentTime = () => {
-    const { isPlaying } = this.state;
-    const { duration } = this.props;
-
-    const timer = setInterval(() => {
-      if (isPlaying) {
-        this.setState({
-          currentTime: Number(this.playerOGV.currentTime.toFixed(5))
-        });
-      }
-      if (Math.ceil(this.playerOGV.currentTime) >= Math.ceil(duration / 1000) || !isPlaying) {
-        clearInterval(timer);
-      }
-    }, 500);
-  };
-
-  ogvPause = () => {
-    if (this.playerOGV) {
-      this.playerOGV.pause();
-    }
-  };
-
   renderPlayPauseButton() {
     const { pending } = this.props;
-    const { error, isPlaying } = this.state;
+    const { error, isPlaying, isDecoded } = this.state;
 
     return (
-      <div>
-        <AudioPlayerButton
-          error={error}
-          pending={Boolean(pending)}
-          isPlaying={isPlaying}
-          onPlay={this.handlePlayClick}
-          onPause={this.handlePauseClick}
-        />
-      </div>
+      <AudioPlayerButton
+        error={error}
+        pending={Boolean(pending) || !Boolean(isDecoded)}
+        isPlaying={isPlaying}
+        onPlay={this.handlePlayClick}
+        onPause={this.handlePauseClick}
+      />
     );
   }
 
   renderPlayerSeeker() {
     const { currentTime, duration } = this.state;
-    const progress = (this.state.currentTime / (duration / 1000)) * 100;
+    const progress = (currentTime / duration) * 100;
     const current = getHumanTime(currentTime);
 
     const className = classNames(styles.seeker, {
-      [styles.seekerError]: this.state.error
+      [styles.seekerError]: this.state.error,
     });
 
     return (
-      <div className={className} onClick={this.handleRewind} ref={this.setRewind} title={current}>
-        <div className={styles.seekerPlayed} style={{ width: progress + '%' }} />
+      <div
+        className={className}
+        onClick={this.handleRewind}
+        ref={this.setRewind}
+        title={current}
+      >
+        <div
+          className={styles.seekerPlayed}
+          style={{ width: progress + '%' }}
+        />
       </div>
     );
   }
 
-  renderAudioElement() {
-    const { src } = this.props;
-    const { key } = this.state;
+  decodeAudio = (buffer) => {
+    const typedArray = new Uint8Array(buffer);
 
-    if (!src) {
+    this.decoderWorker = new Worker(options.decoderWorkerPath);
+    this.wavWorker = new Worker(options.wavWorkerPath);
+
+    this.decoderWorker.postMessage({
+      command:'init',
+      decoderSampleRate: options.sampleRate,
+      outputBufferSampleRate: options.sampleRate
+    });
+
+    this.wavWorker.postMessage({
+      command:'init',
+      wavBitDepth: options.wavBitDepth,
+      wavSampleRate: options.sampleRate
+    });
+
+    this.decoderWorker.onmessage = this.decodeWorkerMessage;
+    this.wavWorker.onmessage = this.wavWorkerMessage;
+
+    this.decoderWorker.postMessage({
+      command: 'decode',
+      pages: typedArray
+    }, [typedArray.buffer] );
+  }
+
+  decodeWorkerMessage = (e) => {
+    if (e.data === null) {
+      this.wavWorker.postMessage({ command: 'done' });
+    } else {
+      this.wavWorker.postMessage({
+        command: 'encode',
+        buffers: e.data
+      }, e.data.map(function(typedArray){
+        return typedArray.buffer;
+      }));
+    }
+  }
+
+  wavWorkerMessage = (e) => {
+    if (e.data !== null) {
+      var dataBlob = new Blob( [ e.data ], { type: "audio/wav" } );
+      var url = URL.createObjectURL( dataBlob );
+
+      this.setState({ decodedSrc: url, isDecoded: true });
+    }
+  }
+
+  renderAudioElement() {
+    const { key, decodedSrc } = this.state;
+
+    if (!decodedSrc) {
       return null;
     }
 
@@ -305,9 +277,10 @@ class AudioPlayer extends PureComponent<Props, State> {
       <audio
         key={key}
         ref={this.setAudio}
-        src={src}
+        src={decodedSrc}
         onError={this.handleError}
         onEnded={this.handleEnded}
+        onPause={this.handlePause}
         onTimeUpdate={this.handleTimeUpdate}
         onLoadedMetadata={this.handleLoadedMetadata}
       />
@@ -320,7 +293,9 @@ class AudioPlayer extends PureComponent<Props, State> {
       return <MediaErrorMessage className={styles.error} error={error} />;
     }
 
-    return <div className={styles.state}>{getHumanTime(this.state.duration)}</div>;
+    return (
+      <div className={styles.state}>{getHumanTime(this.state.duration)}</div>
+    );
   }
 
   renderSender() {
@@ -347,7 +322,7 @@ class AudioPlayer extends PureComponent<Props, State> {
             {this.renderSender()}
           </div>
         </div>
-        {this.state.isOgg ? this.setAudio() : this.renderAudioElement()}
+        {this.renderAudioElement()}
       </div>
     );
   }
