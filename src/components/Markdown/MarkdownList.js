e9b07a2fc86e5bfd23efcdaaa534b161542db4c1
Oleg Shilov <oleg@dlg.im>
2019-06-07 11:59:40 +0300

    fix(Markdown): fix styles for list checkboxes

--- src/components/Markdown/MarkdownList.js
+++ src/components/Markdown/MarkdownList.js
@@ -4,12 +4,13 @@
  */
 
 import type { ListItem } from '@dlghq/markdown/src/types';
-import * as React from 'react';
+import React from 'react';
+import classNames from 'classnames';
 import MarkdownText from './MarkdownText';
-import styles from './Markdown.css';
 import Checkbox from '../Checkbox/Checkbox';
+import styles from './Markdown.css';
 
-type Props = {
+type MarkdownListProps = {
   idPrefix: string,
   content: Array<ListItem>,
   emojiSize: number,
@@ -23,22 +24,41 @@ function MarkdownList({
   emojiSize,
   isEditable,
   onChange,
-}: Props) {
+}: MarkdownListProps) {
   return (
     <ul className={styles.list}>
-      {content.map((item, i) => (
-        <li key={i} className={styles.listItem}>
-          {typeof item.done === 'boolean' ? (
-            <Checkbox
-              id={idPrefix + '_' + i}
-              value={item.done}
-              disabled={!isEditable}
-              onChange={() => onChange(i, !item.done)}
-            />
-          ) : null}
-          <MarkdownText tokens={item.content} emojiSize={emojiSize} isInline />
-        </li>
-      ))}
+      {content.map((item, i) => {
+        const isCheckbox = typeof item.done === 'boolean';
+        const listItemClassName = classNames(styles.listItem, {
+          [styles.listItemBoolean]: isCheckbox,
+        });
+
+        return (
+          <li key={i} className={listItemClassName}>
+            {isCheckbox ? (
+              <Checkbox
+                id={idPrefix + '_' + i}
+                value={item.done}
+                disabled={!isEditable}
+                onChange={() => onChange(i, !item.done)}
+                label={
+                  <MarkdownText
+                    tokens={item.content}
+                    emojiSize={emojiSize}
+                    isInline
+                  />
+                }
+              />
+            ) : (
+              <MarkdownText
+                tokens={item.content}
+                emojiSize={emojiSize}
+                isInline
+              />
+            )}
+          </li>
+        );
+      })}
     </ul>
   );
 }
